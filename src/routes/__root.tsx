8181a547771a950c77006d8259932e691c911777
Jason Savelli <savellijason@gmail.com>
2025-12-18 21:48:05 +0100

    task: update app title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -31,9 +31,8 @@ export const Route = createRootRouteWithContext<{
         content: "width=device-width, initial-scale=1",
       },
       ...seo({
-        title:
-          "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+        title: "SW tool",
+        description: "SW tool let you easily redeem coupon code",
       }),
     ],
     links: [
